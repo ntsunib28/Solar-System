07ab427fc6cd5bda638b365ac7578a7ac6809fa1
nt <nicholast.theodore@binus.ac.id>
2023-11-17 18:03:49 +0700

    Planet bisa memutari matahari

--- index.js
+++ index.js
@@ -1,151 +1,210 @@
 import * as THREE from './Three JS/build/three.module.js'
 import {OrbitControls} from './Three JS/examples/jsm/controls/OrbitControls.js'
 
-let scene, camera1, camera2, currentCam, renderer
+let scene, orbitCam, normalCam, currentCam, renderer, controls
 let sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
 const textureLoader = new THREE.TextureLoader()
-let solarSystem = new THREE.Group()
 
-function init() {
-    scene = new THREE.Scene()
-    scene.background = textureLoader.load("./assets/stars_milky_way.jpg")
+scene = new THREE.Scene()
+scene.background = textureLoader.load("./assets/stars_milky_way.jpg")
+const fov = 45
+const width = window.innerWidth
+const height = window.innerHeight
+const aspect = width / height
+orbitCam = new THREE.PerspectiveCamera(fov, aspect, 0.1, 1000)
+normalCam = new THREE.PerspectiveCamera(fov, aspect, 0.1, 1000)
 
-    const fov = 45
-    const width = window.innerWidth
-    const height = window.innerHeight
-    const aspect = width / height
+orbitCam.position.set(-90,140,140)
+normalCam.position.set(-90,140,140)
+normalCam.lookAt(0,0,0)
+currentCam = normalCam
+renderer = new THREE.WebGLRenderer({antialias: true})
+controls = new OrbitControls(orbitCam, renderer.domElement)
+controls.update()
+document.body.appendChild(renderer.domElement)
 
-    camera1 = new THREE.PerspectiveCamera(fov, aspect, 0.1, 1000)
-    camera2 = new THREE.PerspectiveCamera(fov, aspect, 0.1, 1000)
-   
-    camera1.position.set(0,20,70)
-    camera1.lookAt(0,0,0)
+//create sun
+const sunGeo = new THREE.SphereGeometry(16, 30, 30);
+const sunMat = new THREE.MeshBasicMaterial({
+    map: textureLoader.load("./assets/sun.jpg")
+});
+sun = new THREE.Mesh(sunGeo, sunMat);
+console.log(sun)
+sun.userData = { isClickable: true }
+console.log(sun)
+scene.add(sun);
 
-    camera2.position.set(0,20,70)
-    camera2.lookAt(0,0,0)
+// Sunlight
+const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300);
+scene.add(pointLight);
 
-    currentCam = camera1
+//Light
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
 
-    renderer = new THREE.WebGLRenderer({antialias: true})
-
-    const controls = new OrbitControls(camera1, renderer.domElement)
-
-    document.body.appendChild(renderer.domElement)
-}
-
-function createPlanet(map, radius, widthSegment, heightSegment, x, y, z){
+function createPlanet(map, size, x, ring){
     const texture = textureLoader.load(map)
-    const geo = new THREE.SphereGeometry(radius, widthSegment, heightSegment)
-    const mat = new THREE.MeshBasicMaterial({
+    const geo = new THREE.SphereGeometry(size, 30, 30)
+    const mat = new THREE.MeshStandardMaterial({
         map: texture
     })
     const mesh = new THREE.Mesh(geo, mat)
-    mesh.castShadow = true
-    mesh.receiveShadow = true
-    mesh.position.set(x,y,z)
+    const obj = new THREE.Object3D()
+    obj.add(mesh)
+    
+    if(ring) {
+        const ringGeo = new THREE.RingGeometry(
+            ring.innerRadius,
+            ring.outerRadius,
+            32);
+        const ringMat = new THREE.MeshBasicMaterial({
+            map: textureLoader.load(ring.texture),
+            side: THREE.DoubleSide
+        });
+        ringMat.transparent = true;
+        ringMat.blending = THREE.AdditiveBlending;
+        const ringMesh = new THREE.Mesh(ringGeo, ringMat);
+        obj.add(ringMesh);
+        ringMesh.position.x = x;
+        ringMesh.rotation.x = -0.5 * Math.PI;
+    }
 
+    mesh.position.x = x
     mesh.userData = { isClickable: true }
 
-    return mesh
+    scene.add(obj)
+
+    return {mesh: mesh, obj: obj}
 }
 
-function createRing(map, innerRadius, outerRadius, thetaSegment, x, y, z) {
-    const texture = textureLoader.load(map)
-    const ringGeometry = new THREE.RingGeometry(innerRadius, outerRadius, thetaSegment);
-    const ringMaterial = new THREE.MeshBasicMaterial({
-        map: texture,
-        side: THREE.DoubleSide });
-    const ring = new THREE.Mesh(ringGeometry, ringMaterial);
-    ring.rotateZ = Math.PI/4
-    ring.position.set(x,y,z)
-    scene.add(ring);
-}
 
-function render(){ 
-    requestAnimationFrame(render)
-    renderer.render(scene, currentCam)
-}
 
-let keyListener = event => {
-    let keyCode = event.keyCode
-    if(keyCode == 32){
+
+mercury = createPlanet("./assets/mercury.jpg", 3.2, 28)
+venus = createPlanet("./assets/venus.jpg", 5.8, 44)
+earth = createPlanet("./assets/earth.jpg", 6, 62)
+mars = createPlanet("./assets/mars.jpg", 4, 78)
+jupiter = createPlanet("./assets/jupiter.jpg", 12, 100)
+saturn = createPlanet("./assets/saturn.jpg", 10, 138, {
+    innerRadius: 10,
+    outerRadius: 20,
+    texture: "./assets/saturn_ring.png"
+})
+uranus = createPlanet("./assets/uranus.jpg", 7, 176)
+neptune = createPlanet("./assets/neptune.jpg", 7, 200)
+
+window.addEventListener("keydown", (event)=>{
+    let key = event.key
+    if(key == " "){
         console.log("SPACE")
-        if(currentCam == camera1) currentCam = camera2
-        else currentCam = camera1
+        if(currentCam == orbitCam) currentCam = normalCam
+        else currentCam = orbitCam
     }
-    if(currentCam == camera2){
-        if(keyCode == 87){
+
+    // controls.target = earth.position
+
+    if(currentCam == normalCam){
+        if(key == "w"){
             //w
             currentCam.position.y += 2
-        }else if(keyCode == 65){
+        }else if(key == "a"){
             //a
             currentCam.position.x -= 2
-        }else if(keyCode == 83){
+        }else if(key == "s"){
             //s
             currentCam.position.y -= 2
-        }else if(keyCode == 68){
+        }else if(key == "d"){
             //d
             currentCam.position.x += 2
-        }else if(keyCode == 189){
+        }else if(key == "-"){
             //-(ZOOM OUT)
             currentCam.position.z += 2
-        }else if(keyCode == 187){
+        }else if(key == "="){
             //=(ZOOM IN)
             currentCam.position.z -= 2
         }
     }
     
+})
+
+function animatePlanet() {
+    //Rotasi
+    sun.rotateY(0.004);
+    mercury.mesh.rotateY(0.004);
+    venus.mesh.rotateY(0.002);
+    earth.mesh.rotateY(0.02);
+    mars.mesh.rotateY(0.018);
+    jupiter.mesh.rotateY(0.04);
+    saturn.mesh.rotateY(0.038);
+    uranus.mesh.rotateY(0.03);
+    neptune.mesh.rotateY(0.032);
+
+    //Revolusi
+    mercury.obj.rotateY(0.04);
+    venus.obj.rotateY(0.015);
+    earth.obj.rotateY(0.01);
+    mars.obj.rotateY(0.008);
+    jupiter.obj.rotateY(0.002);
+    saturn.obj.rotateY(0.0009);
+    uranus.obj.rotateY(0.0004);
+    neptune.obj.rotateY(0.0001);
+
+    renderer.render(scene, currentCam);
 }
 
-const EARTH_YEAR = 2 * Math.PI * (1 / 60) * (1 / 60);
+renderer.setAnimationLoop(animatePlanet)
 
-function animateRotation() {
-    sun.rotation.y += 0.001;
-    mercury.rotation.y += EARTH_YEAR * 4;
-    venus.rotation.y += EARTH_YEAR * 2;
-    earth.rotation.y += EARTH_YEAR;
-    mars.rotation.y += EARTH_YEAR * 0.5;
-    jupiter.rotation.y += EARTH_YEAR * 4;
-    saturn.rotation.y += EARTH_YEAR * 2;
-    uranus.rotation.y += EARTH_YEAR;
-    neptune.rotation.y += EARTH_YEAR * 0.5;
+animatePlanet()
+resize()
 
-    requestAnimationFrame(animateRotation)
-}
-
-function solarSystemAdd() {
-    solarSystem.add(sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune)
-}
-
-window.onload = () => {
-    init()
-    sun = createPlanet("./assets/sun.jpg", 8, 64, 64, 0, 0, 0)
-    mercury = createPlanet("./assets/mercury.jpg", 2, 64, 64, 20, 0, 0)
-    venus = createPlanet("./assets/venus.jpg", 3, 64, 64, 40, 0, 0)
-    earth = createPlanet("./assets/earth.jpg", 4, 64, 64, 60, 0, 0)
-    mars = createPlanet("./assets/mars.jpg", 3, 64, 64, 80, 0, 0)
-    jupiter = createPlanet("./assets/jupiter.jpg", 4, 64, 64, 100, 0, 0)
-    saturn = createPlanet("./assets/saturn.jpg", 3, 64, 64, 120, 0, 0)
-    createRing("./assets/saturn_ring.png", 5, 5.05, 32, 120, 0, 0)
-    uranus = createPlanet("./assets/uranus.jpg", 4, 64, 64, 140, 0, 0)
-    neptune = createPlanet("./assets/neptune.jpg", 3, 64, 64, 160, 0, 0)
-
-    solarSystemAdd()
-
-    scene.add(solarSystem)
-
-    animateRotation()
-
-    document.addEventListener("keydown", keyListener)
-
-    render()
-    resize()
-}
 
 function resize(){
     renderer.setSize(window.innerWidth, window.innerHeight)
     currentCam.aspect = window.innerWidth / window.innerHeight
     currentCam.updateProjectionMatrix()
 }
-
 window.addEventListener("resize", resize)
+
+const raycaster = new THREE.Raycaster()
+const pointer = new THREE.Vector2()
+const intersects = raycaster.intersectObjects(scene.children, true)
+
+window.addEventListener("onmove", event =>{
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera(pointer, currentCam)
+    intersects = raycaster.intersectObjects(scene.children)
+    
+    if (intersects.length > 0) {
+        const clickableObject = intersects[0].object;
+        if (clickableObject.userData.isClickable) {
+          document.body.style.cursor = 'pointer';
+        } else {
+          document.body.style.cursor = 'default';
+        }
+      } else {
+        document.body.style.cursor = 'default';
+      }
+})
+
+function onPlanetClick(){
+    console.log(intersects[0].object)
+    console.log("clicked")
+}
+
+window.addEventListener('click', event =>{
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera(pointer, currentCam)
+
+    // Check for intersections
+    if (intersects.length > 0 && intersects[0].object.userData.isClickable) {
+        intersects[0].object.userData = { isClickable: false }
+        onPlanetClick()
+    } else {
+      document.body.style.cursor = 'default'
+    }
+
+})

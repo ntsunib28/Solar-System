4aa5b504c2ea28b34278b2ef656412b21fed934b
Kobel <nicholas.williem@binus.ac.id>
2023-12-24 15:58:08 +0700

    semua satelit

--- jupiter.js
+++ jupiter.js
@@ -2,7 +2,7 @@ import * as THREE from './Three JS/build/three.module.js'
 
 const aspect = window.innerWidth / window.innerHeight
 const cam = new THREE.PerspectiveCamera(70,aspect,1,1000)
-cam.position.set(0, 0, 20)
+cam.position.set(-18, 28, 45)
 cam.lookAt(0,0,0)
 
 const scene = new THREE.Scene()
@@ -21,16 +21,57 @@ function createPlanet(map, size, x) {
     })
     const mesh = new THREE.Mesh(geo, mat)
     scene.add(mesh)
+    const obj = new THREE.Object3D()
+    obj.add(mesh)
     mesh.position.x = x
+    scene.add(obj)
 
-    return {mesh: mesh}
+    return { mesh: mesh, obj: obj }
 }
 
 const jupiter = createPlanet("./assets/jupiter.jpg", 6, 0);
 
+function createSatelite(size, x, y, z, map) {
+    const sateliteTexture = new THREE.TextureLoader().load(map)
+    const sateliteGeo = new THREE.SphereGeometry(size, 30, 30)
+    const sateliteMat = new THREE.MeshStandardMaterial({
+        map: sateliteTexture
+    })
+    const sateliteMesh = new THREE.Mesh(sateliteGeo, sateliteMat)
+    scene.add(sateliteMesh)
+    const sateliteObj = new THREE.Object3D()
+    sateliteObj.add(sateliteMesh)
+
+    sateliteMesh.position.set(x, y, z)
+    jupiter.obj.add(sateliteObj)
+
+    return { mesh: sateliteMesh, obj: sateliteObj }
+}
+
+const ganymede = createSatelite(2.641, 15, 0, 0, './assets/ganymede.jpg')
+const callisto = createSatelite(2.41, 25, 0, 0, './assets/callisto.jpg')
+const io = createSatelite(1.821, 35, 0, 0, './assets/io.jpg')
+const europa = createSatelite(1.821, 45, 0, 0, './assets/europa.jpg')
+
 function render() {
     jupiter.mesh.rotateY(0.002)
     jupiter.mesh.rotateX(0.0002)
+
+    ganymede.mesh.rotateY(-0.001)
+    ganymede.mesh.rotateX(0.0005)
+    ganymede.obj.rotateY(0.004)
+
+    callisto.mesh.rotateY(-0.001)
+    callisto.mesh.rotateX(0.0005)
+    callisto.obj.rotateY(0.003)
+
+    io.mesh.rotateY(-0.001)
+    io.mesh.rotateX(0.0005)
+    io.obj.rotateY(0.002)
+
+    europa.mesh.rotateY(-0.001)
+    europa.mesh.rotateX(0.0005)
+    europa.obj.rotateY(0.001)
     renderer.render(scene, cam)
 }
 
--- uranus.js
+++ uranus.js
@@ -2,7 +2,7 @@ import * as THREE from './Three JS/build/three.module.js'
 
 const aspect = window.innerWidth / window.innerHeight
 const cam = new THREE.PerspectiveCamera(70,aspect,1,1000)
-cam.position.set(0, 0, 20)
+cam.position.set(-18, 28, 40)
 cam.lookAt(0,0,0)
 
 const scene = new THREE.Scene()
@@ -21,16 +21,62 @@ function createPlanet(map, size, x) {
     })
     const mesh = new THREE.Mesh(geo, mat)
     scene.add(mesh)
+    const obj = new THREE.Object3D()
+    obj.add(mesh)
     mesh.position.x = x
+    scene.add(obj)
 
-    return {mesh: mesh}
+    return { mesh: mesh, obj: obj }
 }
 
 const uranus = createPlanet("./assets/uranus.jpg", 6, 0);
 
+function createSatelite(map, x, y, z, size) {
+    const sateliteTexture = new THREE.TextureLoader().load(map)
+    const sateliteGeo = new THREE.SphereGeometry(size, 30, 30)
+    const sateliteMat = new THREE.MeshStandardMaterial({
+        map: sateliteTexture
+    })
+    const sateliteMesh = new THREE.Mesh(sateliteGeo, sateliteMat)
+    scene.add(sateliteMesh)
+    const sateliteObj = new THREE.Object3D()
+    sateliteObj.add(sateliteMesh)
+
+    sateliteMesh.position.set(x, y, z)
+    uranus.obj.add(sateliteObj)
+
+    return { mesh: sateliteMesh, obj: sateliteObj }
+}
+
+const miranda = createSatelite('./assets/miranda.jpg', 8.64, 0, 0, 0.968);
+const ariel = createSatelite('./assets/ariel.jpg', 24.93, 0, 0, 0.728);
+const umbriel = createSatelite('./assets/umbriel.jpg', 32.62, 0, 0, 0.586);
+const titania = createSatelite('./assets/titania.jpg', 18.24, 0, 0, 0.4888);
+const oberon = createSatelite('./assets/oberon.jpg', 21.78, 0, 0, 0.4092);
+
 function render() {
     uranus.mesh.rotateY(0.002)
     uranus.mesh.rotateX(0.0002)
+
+    miranda.mesh.rotateY(-0.001)
+    miranda.mesh.rotateX(0.0005)
+    miranda.obj.rotateY(0.004)
+
+    ariel.mesh.rotateY(-0.001)
+    ariel.mesh.rotateX(0.0005)
+    ariel.obj.rotateY(0.003)
+
+    umbriel.mesh.rotateY(-0.001)
+    umbriel.mesh.rotateX(0.0005)
+    umbriel.obj.rotateY(0.0025)
+
+    titania.mesh.rotateY(-0.001)
+    titania.mesh.rotateX(0.0005)
+    titania.obj.rotateY(0.0015)
+
+    oberon.mesh.rotateY(-0.001)
+    oberon.mesh.rotateX(0.0005)
+    oberon.obj.rotateY(0.001)
     renderer.render(scene, cam)
 }
 
